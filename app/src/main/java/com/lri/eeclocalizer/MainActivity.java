b04457a8c5ca6dfacf09dd6054345a1c97940906
Lyonnel Dzotang <lyonnel.dzotang@wouri.co>
2015-12-07 14:59:34 +0100

    add a feature of draw a route between user location to a marker

--- app/src/main/java/com/lri/eeclocalizer/MainActivity.java
+++ app/src/main/java/com/lri/eeclocalizer/MainActivity.java
@@ -1,10 +1,13 @@
 package com.lri.eeclocalizer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.go);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GoActivity.class));
+            }
+        });
     }
 
 
