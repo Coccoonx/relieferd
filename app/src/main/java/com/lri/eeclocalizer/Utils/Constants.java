d74da79b746c38c86249cf0535b036f97fd89141
Lyonnel Dzotang <lyonnel.dzotang@wouri.co>
2015-12-07 15:07:18 +0100

    add utils file

---
+++ app/src/main/java/com/lri/eeclocalizer/Utils/Constants.java
@@ -0,0 +1,10 @@
+package com.lri.eeclocalizer.Utils;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by borelkoumo on 07/12/15.
+ */
+public interface Constants {
+//    LatLng eectamdja = new LatLng();
+}
---
+++ app/src/main/java/com/lri/eeclocalizer/Utils/CoreUtils.java
@@ -0,0 +1,13 @@
+package com.lri.eeclocalizer.Utils;
+
+import java.util.List;
+
+/**
+ * Created by borelkoumo on 07/12/15.
+ */
+public class CoreUtils {
+
+    public static List<String> getPoints() {
+        return null;
+    }
+}
